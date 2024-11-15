166052daf01a350160be043c1b1f19b6b540a286
herve guerre <herveguerre@hotmail.fr>
2024-11-15 09:30:34 +0100

    m

--- footer.js
+++ footer.js
@@ -7,3 +7,19 @@ function inclureFooter() {
       });
   }
   inclureFooter();
+
+
+  // Remplace l'image par le logo si elle n'est pas disponible
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll("img"); // Sélectionne toutes les images
+
+  images.forEach((image) => {
+    // Ajoute l'attribut lazy loading à chaque image
+    image.setAttribute("loading", "lazy");
+
+    // Remplace l'image par le logo si elle n'est pas disponible
+    image.onerror = function () {
+      this.src = "/G.png";
+    };
+  });
+});
